5f1d0917c3cbf3c749857d88221b68340a7ea567
Corey Sutton <corey.sutton@datacom.co.nz>
2021-12-29 19:56:54 +1300

    Update settings

--- TogglTools/Core/Settings.cs
+++ TogglTools/Core/Settings.cs
@@ -2,7 +2,7 @@
 {
     public struct Settings
     {
-        public const string TogglApiDomain = @"https://toggl.com";
+        public const string TogglApiDomain = @"https://api.track.toggl.com";
         public const string TogglApiWorkspaceEndpoint = @"/api/v8/workspaces";
         public const string TogglApiReportEndpoint = @"/reports/api/v2/details";
         public const string CustomApiDomain = @"http://localhost:53343";
